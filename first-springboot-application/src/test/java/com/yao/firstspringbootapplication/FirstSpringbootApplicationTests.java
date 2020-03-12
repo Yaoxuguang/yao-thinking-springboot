84bc5c22ac1b5675b0e0ba1d046d539bea0d8585
yaoxuguang <tivenninesongs@163.com>
2020-03-12 10:54:59 +0800

    first commit

---
+++ first-springboot-application/src/test/java/com/yao/firstspringbootapplication/FirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao.firstspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot-mavendependency/src/main/java/com/yao/springbootmavendependency/SpringbootMavendependencyApplication.java
@@ -0,0 +1,13 @@
+package com.yao.springbootmavendependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavendependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavendependencyApplication.class, args);
+    }
+
+}
